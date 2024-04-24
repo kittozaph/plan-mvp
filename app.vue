40f90ebf42bb7250d9bff4165271c2879fcc1329
Fikri <fikfan@gmail.com>
2024-04-24 11:33:16 +0800

    project init

--- app.vue
+++ app.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <div class="text-red-500 text-xl font-bold">Hello</div>
 </template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
