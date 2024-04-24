9e698531fc8f932d68554664d30e74d4575ab6e4
kittodesu <118090806+kittozaph@users.noreply.github.com>
2024-04-24 14:01:48 +0800

    Update components.vue

--- pages/components.vue
+++ pages/components.vue
@@ -19,6 +19,25 @@
             </div>
         </div>
     </div>
+
+    <div class="card-details">
+        <div class="card-title">
+            <h3>Rsv recovery</h3>
+            <div class="card-price">$85</div>
+        </div>
+        <div class="card-info">
+            <i class="fa fa-laptop" style="font-size:12px"></i> <!-- Replace with your desired icon class -->
+            <span>Remote</span>
+        </div>
+        <div class="card-info">
+            <i class="fa fa-calendar" style="font-size:12px"></i>
+            <span>Before Fri, 19 Apr</span>
+        </div>
+        <div class="card-action">
+            <span>Open</span>
+            <i class="fa fa-user" style="font-size:24px; margin-left: auto;"></i> <!-- Replace with your desired icon class -->
+        </div>
+    </div>
   
 </template>
 
@@ -29,5 +48,54 @@ export default {
 </script>
 
 <style>
+.card-details {
+    border: 1px solid #e2dfe4;
+    background-color: rgb(245, 242, 248);
+    border-radius: 5px;
+    padding: 1px 5px 20px 10px;
+    margin: 10px;
+    width: 290px;
+    box-shadow: 0 2px 4px #d8c5e2e5;
+}
 
+.card-title {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.card-price {
+    font-weight: bold;
+    font-size: 16px;
+    margin-left: auto;
+}
+
+.card-info {
+    display: flex;
+    align-items: center;
+    margin-bottom: 5px;
+    margin-right: 5px;
+}
+
+.card-info i {
+    margin-right: 5px;
+}
+
+.card-action {
+    display: flex;
+    justify-content: left;
+    margin-top: 10px;
+
+}
+
+.card-action i {
+    margin-left: auto;
+    margin-right: 5px;
+}
+
+.card-action span {
+    font-size: 16px;
+    font-weight: bold; /* This will make the text bold */
+    padding: auto;
+}
 </style>
